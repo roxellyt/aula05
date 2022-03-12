6363d478b237a6a0a97168742005922cdc92e7ab
roxellyt <roxelly.tavares@gmail.com>
2022-03-11 21:13:08 -0300

    3 files modified, 3 added

--- src/app/list-details/list-details.component.ts
+++ src/app/list-details/list-details.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-details',
   templateUrl: './list-details.component.html',
-  styleUrls: ['./list-details.component.css']
+  styleUrls: ['./list-details.component.css'],
 })
 export class ListDetailsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
