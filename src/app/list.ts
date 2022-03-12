d9057bf1a812a221105d61bb4a563c5ace9a7b77
roxellyt <roxelly.tavares@gmail.com>
2022-03-11 21:51:06 -0300

    2 files modified, 1 added

---
+++ src/app/list.ts
@@ -0,0 +1,5 @@
+export const ListData = [
+  { id: 1, text: 'Disciplina' },
+  { id: 2, text: 'Ementa' },
+  { id: 3, text: 'Objetivos' }
+];
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListData } from '../list';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  list = ListData;
   constructor() {}
 
   ngOnInit() {}
