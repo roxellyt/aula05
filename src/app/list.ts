beb372798c9a6640b8be96744453a677ea813c09
roxellyt <roxelly.tavares@gmail.com>
2022-03-11 22:07:32 -0300

    2 files modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,5 +1,5 @@
 export const ListData = [
-  { id: 1, text: 'Disciplina' },
-  { id: 2, text: 'Ementa' },
-  { id: 3, text: 'Objetivos' }
+  { id: 1, text: 'Disciplina: Tópicos Especiais em Sistemas para Internet ll' },
+  { id: 2, text: 'Ementa: Recursos e aplicações da tecnologia escolhida' },
+  { id: 3, text: 'Objetivos: Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade. Objetivos específicos. Estudar e aplicar tecnologias de uso corrente na região na qual o curso de insere ou de interesse para o desenvolvimento profissional dos estudantes ou desenvolver temas que aprofundem algum conhecimento já estudado' }
 ];
